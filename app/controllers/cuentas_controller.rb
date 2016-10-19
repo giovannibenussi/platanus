d3a1a30ab19c1b5dc8116249859efb80ba4d8063
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-10-19 19:53:02 -0300

    GET /cuentas/direccion crea cuenta si esta no existe

--- app/controllers/cuentas_controller.rb
+++ app/controllers/cuentas_controller.rb
@@ -1,51 +1,62 @@
+require 'digest'
+
 class CuentasController < ApplicationController
-  before_action :set_cuenta, only: [:show, :update, :destroy]
+    before_action :set_cuenta, only: [:update, :destroy]
 
-  # GET /cuentas
-  def index
-    @cuentas = Cuenta.all
+    # GET /cuentas
+    def index
+        @cuentas = Cuenta.all
 
-    render json: @cuentas
-  end
-
-  # GET /cuentas/1
-  def show
-    render json: @cuenta
-  end
-
-  # POST /cuentas
-  def create
-    @cuenta = Cuenta.new(cuenta_params)
-
-    if @cuenta.save
-      render json: @cuenta, status: :created, location: @cuenta
-    else
-      render json: @cuenta.errors, status: :unprocessable_entity
+        render json: @cuentas
     end
-  end
 
-  # PATCH/PUT /cuentas/1
-  def update
-    if @cuenta.update(cuenta_params)
-      render json: @cuenta
-    else
-      render json: @cuenta.errors, status: :unprocessable_entity
+    # GET /cuentas/1
+    def show
+        direccion = Digest::SHA256.base64digest params[:id]
+        @cuenta = Cuenta.find_by 'direccion', direccion
+        if @cuenta.nil?
+            @cuenta = Cuenta.new
+            @cuenta.direccion = direccion
+            @cuenta.saldo     = 0
+            @cuenta.save!
+        end
+        render json: @cuenta
     end
-  end
 
-  # DELETE /cuentas/1
-  def destroy
-    @cuenta.destroy
-  end
+    # POST /cuentas
+    def create
+        @cuenta = Cuenta.new(cuenta_params)
 
-  private
+        if @cuenta.save
+            render json: @cuenta, status: :created, location: @cuenta
+        else
+            render json: @cuenta.errors, status: :unprocessable_entity
+        end
+    end
+
+    # PATCH/PUT /cuentas/1
+    def update
+        if @cuenta.update(cuenta_params)
+            render json: @cuenta
+        else
+            render json: @cuenta.errors, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /cuentas/1
+    def destroy
+        @cuenta.destroy
+    end
+
+    private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_cuenta
-      @cuenta = Cuenta.find(params[:id])
+        @cuenta = Cuenta.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def cuenta_params
-      params.require(:cuenta).permit(:direccion, :saldo)
+        params.require(:cuenta).permit(:direccion, :saldo)
     end
 end
